c23e46f232b46677287e0da4200aeb75e39269a8
marioangst <mario.angst@gmail.com>
2017-09-29 13:22:34 +0200

    csv with no linebreaks (screws up excel)

--- write_html_laws_to_csv.py
+++ write_html_laws_to_csv.py
@@ -55,6 +55,24 @@ def write_soup_law_to_articles_list(soup_object):
                 
     return(law_list)
 
+def write_soup_law_to_articles_list_nobreaks(soup_object):
+
+    law_list = []
+    
+    for lawcontent in soup_object.find_all("div", {"id": "lawcontent"}):
+        for children in lawcontent.find_all(recursive = True): #recursive = False to only get the next level children
+            if children.name == "h5": #h5 tags are around article headers
+                if children.nextSibling.attrs['class'] != ['collapseableArticle']: #make sure after the article header comes law text
+                    print("\n ***** no article text after article title")
+                    break
+                paragraphs_list = [paragraph.get_text() for paragraph in children.nextSibling.find_all("p")]
+                article_list_entry = {'article_number': children.strong.string,
+                                  'article_title': children.get_text(),
+                                  'article_text': " ".join(paragraphs_list)}
+                law_list.append(article_list_entry)
+                
+    return(law_list)
+
 #test_law_list = write_soup_law_to_articles_list(soup)
 #print(test_law_list[5]['article_text'])
 
@@ -85,7 +103,7 @@ def write_articles_list_to_csv(article_list):
 for file in filepath_list:
     with open(file, encoding = "UTF-8") as fp:
         soup = BeautifulSoup(fp) #make soup object  
-    list_of_articles = write_soup_law_to_articles_list(soup)
+    list_of_articles = write_soup_law_to_articles_list_nobreaks(soup)
     write_articles_list_to_csv(list_of_articles)
  
 
